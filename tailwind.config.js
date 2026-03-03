71ea5e5d3766c320f9d367a46969cb02b1cbd38b
IZI1996 <izikaoutar@gmail.com>
2026-03-03 13:16:57 +0100

    feat: Angular 19 landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-/** 
- * @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -14,12 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: { extend: {} },
   plugins: [],
 }
